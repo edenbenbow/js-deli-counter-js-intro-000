ecc6bd0cac80f3694d8644ca4d7dbdc0f7c43dbb
E.B <edenbenbow@yahoo.com>
2019-07-31 02:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function takeANumber (katzDeliLine, name){
 return "Welcome, " + name +". You are number "+ position +" in line."
 
 }
+
+function nowServing(katzDeliLine) {
+  return("There is nobody waiting to be served!")
+}
