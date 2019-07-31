b0c84d0ef38242d4073572690dc02f5a9326babd
E.B <edenbenbow@yahoo.com>
2019-07-31 03:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,6 @@ function currentLine() {
   if(katzDeliLine.length === 0) {
     return "The line is currently empty.")
   }
-return("The line is currently: katzDeliLine + "." + name.")
+  if (katzDeliLine.length > 0) {
+return("The line is currently: katzDeliLine + "." + name.")}
 }
