9f240be5a5ab44d7ac23c8e748b75daaaf13ee31
E.B <edenbenbow@yahoo.com>
2019-07-31 03:02:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ return "Welcome, " + name +". You are number "+ position +" in line."
 }
 
 function nowServing(katzDeliLine, name) {
-  while (katzDeliLine < 1) { 
+  while (katzDeliLine < 1) {
     return("There is nobody waiting to be served!")}
 }
 }
