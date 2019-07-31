c3a18e1e4c23f184ccb4f38064712e61adff3f76
E.B <edenbenbow@yahoo.com>
2019-07-31 02:33:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var name = ["Grace"]
+var number = ["4"];
+
 function takeANumber(number, name) {
-  var name = ["Grace"]
-  var number = ["4"];
 return(`Welcome, ${name}. You are number ${number} in line.`)
 }
