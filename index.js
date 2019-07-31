87dc9b7f320b37274662081bfd84742b6e30d901
E.B <edenbenbow@yahoo.com>
2019-07-31 03:34:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,15 +17,14 @@ return ("Currently serving " + name + ".")
 }
 
 
-function currentLine() {
-  var name;
-  var katzDeliLine;
-  if (katzDeliLine.length > 0){
-return ("The line is currently: " + katzDeliLine + '.' + name.")
-}
-
-if (katzDeliLine.length === 0) {
-    return ("The line is currently empty.")
+function currentLine(line) {
+  var lineNum = [];
+  if (line.length == 0) {
+    return "The line is currently empty.";
   }
+  for (var i = 0; i < line.length; i++) {
+    lineNum.push(` ${i+1}. ${line[i]}`);
+ }
+  return "The line is currently:" + lineNum;
 
-}
+ }
